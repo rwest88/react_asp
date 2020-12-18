7cc2575578ebf8f82259025bc50b28875cb07bed
Ryan Weston <rwest88.va@gmail.com>
2020-12-18 09:54:02 -0500

    changed typescript dependency version, write to server-side console

--- react_asp/Controllers/WeatherForecastController.cs
+++ react_asp/Controllers/WeatherForecastController.cs
@@ -27,6 +27,7 @@ namespace react_asp.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
+            Console.WriteLine("API hit!");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
